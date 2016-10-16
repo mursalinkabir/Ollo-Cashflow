dcee022991416599e221d9c6590eca644a79b350
mursalin_kabir <mail2mursalin@gmail.com>
2016-10-16 12:24:33 +0600

    Excel Report is Working

--- Ollo-Cashflow/Controllers/ReportController.cs
+++ Ollo-Cashflow/Controllers/ReportController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using Ollo_Cashflow.Core.BLL;
 
 namespace Ollo_Cashflow.Controllers
@@ -24,6 +27,28 @@ namespace Ollo_Cashflow.Controllers
                 ViewBag.GetAllReportData = reportManager.GetallReportdata();
             }
             
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Report(string test)
+        {
+            ViewBag.isReportDataProcessed = reportManager.ProcessReportData();
+            if (reportManager.ProcessReportData())
+            {
+                ViewBag.GetAllReportData = reportManager.GetallReportdata();
+            }
+            
+            return View();
+        }
+
+        public ActionResult Export()
+        {
+            ViewBag.GetAllReportData = reportManager.GetallReportdata();
+           Response.AddHeader("Content-Type", "application/vnd.ms-excel");
+           Response.AddHeader("Content-Disposition", "attachment; filename=TreasuryReport.xls");
+            Response.ContentEncoding = Encoding.UTF8;
+            //Response.ContentType = "application/ms-excel";
+           
             return View();
         }
     }
